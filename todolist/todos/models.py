f5252a22fa54739f6b1701ef794ae9868a77ee74
yvzkr <yavuzkuru3@gmail.com>
2019-11-13 02:45:57 -0800

    Create todolist table

--- todolist/todos/models.py
+++ todolist/todos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Todos(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.title
