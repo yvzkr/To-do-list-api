613700b2e53fb6f527dce9116bcdb521518a3bd3
yvzkr <yavuzkuru3@gmail.com>
2019-11-14 06:15:55 -0800

    add relation todo's model witg user.

--- todolist/todos/api.py
+++ todolist/todos/api.py
@@ -5,9 +5,15 @@ from .serializers import TodosSerializer
 #Todos Viewset
 
 class TodosViewSet(viewsets.ModelViewSet):
-    queryset = Todos.objects.all()
-    permission_classes = [
-        permissions.AllowAny
-    ]
-
     serializer_class = TodosSerializer
+    
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = TodosSerializer
+
+    def get_queryset(self):
+        return self.request.user.todos.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
--- todolist/todos/models.py
+++ todolist/todos/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Todos(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, related_name="todos", on_delete=models.CASCADE, null=True)
+    
     created_at = models.DateTimeField(auto_now_add=True)
 
     def _str_(self):
