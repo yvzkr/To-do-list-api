deb06beceb63f0464682200c04a949df4f8a0133
yvzkr <yavuzkuru3@gmail.com>
2019-11-14 07:22:26 -0800

    added register login

--- todolist/frontend/src/components/App.js
+++ todolist/frontend/src/components/App.js
@@ -1,5 +1,11 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
+import {
+    HashRouter as Router,
+    Route,
+    Switch,
+    Redirect
+} from "react-router-dom";
 
 import { Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
@@ -7,6 +13,8 @@ import AlertTemplate from "react-alert-template-basic";
 import Header from "./layout/Header";
 import Dashboard from "./todos/Dashboard";
 import Alerts from "./layout/Alerts";
+import Login from "./accounts/Login";
+import Register from "./accounts/Register";
 
 import { Provider } from "react-redux";
 import store from "../store";
@@ -23,13 +31,20 @@ class App extends Component {
         return (
             <Provider store={store}>
                 <AlertProvider template={AlertTemplate} {...alertOptions}>
-                    <Fragment>
-                        <Header />
-                        <Alerts />
-                        <div className="container">
-                            <Dashboard />
-                        </div>
-                    </Fragment>
+                    <Router>
+                        <Fragment>
+                            <Header />
+                            <Alerts />
+                            <div className="container">
+                                <Switch>
+                                    <Route exact path="/" component={Dashboard} />
+                                    <Route exact path="/register" component={Register} />
+                                    <Route exact path="/login" component={Login} />
+
+                                </Switch>
+                            </div>
+                        </Fragment>
+                    </Router>
                 </AlertProvider>
             </Provider>
 
--- todolist/frontend/src/components/layout/Header.js
+++ todolist/frontend/src/components/layout/Header.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 export class Header extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <a className="navbar-brand" href="#">TodoList Manager</a>
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <div className="container">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <a className="navbar-brand" href="#">Yapılacaklar</a>
+                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li className="nav-item">
+                                <Link to="/register" className="nav-link">Kayıt</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/login" className="nav-link">Giriş</Link>
+                            </li>
 
 
-                    </ul>
-
+                        </ul>
+                    </div>
                 </div>
             </nav>
         )
