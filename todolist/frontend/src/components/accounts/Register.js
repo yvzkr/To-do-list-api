2a5c2909f59d2d0916049d425e38a2d9894a76ce
yvzkr <yavuzkuru3@gmail.com>
2019-11-15 03:22:14 -0800

    redirect after register

--- todolist/frontend/src/components/accounts/Register.js
+++ todolist/frontend/src/components/accounts/Register.js
@@ -39,6 +39,10 @@ export class Register extends Component {
     onChange = e => this.setState({ [e.target.name]: e.target.value });
 
     render() {
+        if (this.props.isAuthenticated) {
+            return <Redirect to="/" />
+        }
+
         const { username, email, password, password2 } = this.state;
         return (
             <div className="col-md-6 m-auto">
